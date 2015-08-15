80288007f911a6a376b100b62b02c9f0c2bf823b
Filipe Duarte <me@fmendo.com>
2015-08-15 22:15:35 +0100

    book items in update feed

---
+++ MyShelf/ViewModels/BookViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Mendo.UAP.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShelf.API.XML;
+
+namespace MyShelf.ViewModels
+{
+    public class BookViewModel : ViewModelBase
+    {
+        private Book book;
+
+        public BookViewModel(Book book)
+        {
+            this.book = book;
+        }
+    }
+}
--- MyShelf/ViewModels/UpdateViewModel.cs
+++ MyShelf/ViewModels/UpdateViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Mendo.UAP.Common;
+using MyShelf.API.Services;
 using MyShelf.API.XML;
 using System;
 using System.Collections.Generic;
@@ -15,12 +16,17 @@ namespace MyShelf.ViewModels
         public string UserName { get; set; }
         //public string Link { get; set; }
         public string ImageUrl { get; set; }
+        public bool IsBook { get; set; } = false;
         //public User Actor { get; set; }
         //public string UpdatedAt { get; set; }
         //public UpdateObject Object { get; set; }
         //public string Type { get; set; }
         //public UpdateAction Action { get; set; }
         //public string Body { get; set; }
+
+        public string BookImageUrl { get; set; }
+        public string BookAuthor { get; set; }
+        public string BookTitle { get; set; }
 
         public UpdateViewModel(Update update)
         {
@@ -30,6 +36,37 @@ namespace MyShelf.ViewModels
             ImageUrl = update.Actor?.ImageUrl;
             UserName = update.Actor?.Name;
             ActionText = update.ActionText.Contains("<") ? Regex.Replace(update.ActionText, "<.*?>", string.Empty) : update.ActionText;
+
+            if (update.Type.Equals("review") && update.Object?.Book != null)
+                SetUpBookData(update.Object.Book);
+            else if (update.Type.Equals("readstatus") && update.Object?.Read_status?.Review?.Book != null)
+                SetUpBookData(update.Object.Read_status.Review.Book);
+            else
+                IsBook = false;
+        }
+
+        private void SetUpBookData(Book book)
+        {
+            IsBook = true;
+
+            //BookId = book.Id;
+            //AuthorId = book.AuthorId;
+            //WorkId = book.Work_id;
+
+            GetBookInfoAsync(book);
+        }
+
+        private async Task GetBookInfoAsync(Book book)
+        {
+            var result = await BookService.Instance.GetBookInfo(book.Id);
+
+            BookImageUrl = result.Image_url;
+            BookAuthor = string.Join(", ", result.Authors.Select(a => a.Name));
+            BookTitle = result.Title;
+
+            OnPropertyChanged("BookImageUrl");
+            OnPropertyChanged("BookAuthor");
+            OnPropertyChanged("BookTitle");
         }
     }
 }
