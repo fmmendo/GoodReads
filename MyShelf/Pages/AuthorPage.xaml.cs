7893da31a7bcf297676478c42429b01e0d51c8ff
Filipe Duarte <fmmendo@live.com>
2017-02-11 14:24:01 +0000

    updated ui on user page
    also, fixed ads

--- MyShelf/Pages/AuthorPage.xaml.cs
+++ MyShelf/Pages/AuthorPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Mendo.UWP.Common;
+using Microsoft.Advertising.WinRT.UI;
 using MyShelf.ViewModels;
 using System.Collections.Generic;
 using Windows.UI.Xaml.Navigation;
@@ -14,9 +15,52 @@ namespace MyShelf.Pages
     {
         public AuthorViewModel ViewModel {get;set;}
 
+        public bool ShowAds => !API.Storage.MyShelfSettings.Instance.DontShowAds;
+
+        private const string TEST_APPLICATIONID = "3f83fe91-d6be-434d-a0ae-7351c5a997f1";
+        private const string TEST_ADUNITID = "10865270";
+
+        private const string WAPPLICATIONID = "9a9a9329-8a35-4262-8581-86f3042f9433";
+        private const string WADUNITID = "335544";
+        private const int AD_WIDTH = 300;
+        private const int AD_HEIGHT = 250;
+
+        private const string MAPPLICATIONID = "b5ffff7c-663f-4650-9377-de4b6b20fe7a";
+        private const string MADUNITID = "335545";
+        private const int AD_WIDTH_MOBILE = 480;
+        private const int AD_HEIGHT_MOBILE = 80;
+
+        private AdControl adControl = null;
+
         public AuthorPage()
         {
             this.InitializeComponent();
+
+            SetUpAds();
+        }
+
+        private void SetUpAds()
+        {
+            // Initialize the AdControl.
+            adControl = new AdControl();
+
+            if (DeviceInformation.Instance.IsPhone)
+            {
+                adControl.ApplicationId = MAPPLICATIONID;
+                adControl.AdUnitId = MADUNITID;
+                adControl.Width = AD_WIDTH_MOBILE;
+                adControl.Height = AD_HEIGHT_MOBILE;
+            }
+            else
+            {
+                adControl.ApplicationId = WAPPLICATIONID;
+                adControl.AdUnitId = WADUNITID;
+                adControl.Width = AD_WIDTH;
+                adControl.Height = AD_HEIGHT;
+            }
+            adControl.IsAutoRefreshEnabled = true;
+
+            adGrid.Children.Add(adControl);
         }
 
         protected override void LoadState(object parameter, Dictionary<string, object> pageState)
--- MyShelf/Pages/BookPage.xaml.cs
+++ MyShelf/Pages/BookPage.xaml.cs
@@ -18,19 +18,21 @@ namespace MyShelf.Pages
     {
         public BookViewModel ViewModel { get; set; }
         public bool ShowAds => !API.Storage.MyShelfSettings.Instance.DontShowAds;
+        
+        private const string TEST_APPLICATIONID = "3f83fe91-d6be-434d-a0ae-7351c5a997f1";
+        private const string TEST_ADUNITID = "10865270";
 
+        private const string WAPPLICATIONID = "9a9a9329-8a35-4262-8581-86f3042f9433";
+        private const string WADUNITID = "335544";
         private const int AD_WIDTH = 728;
         private const int AD_HEIGHT = 90;
-        //private const int AD_WIDTH = 480;
-        //private const int AD_HEIGHT = 80;
-        private const string WAPPLICATIONID = "<tbd>";
-        private const string WADUNITID = "<tbd>";
-        private const string MAPPLICATIONID = "<tbd>";
-        private const string MADUNITID = "<tbd>";
+
+        private const string MAPPLICATIONID = "b5ffff7c-663f-4650-9377-de4b6b20fe7a";
+        private const string MADUNITID = "335545";
+        private const int AD_WIDTH_MOBILE = 480;
+        private const int AD_HEIGHT_MOBILE = 80;
 
         private AdControl adControl = null;
-        private string myAppId = WAPPLICATIONID;
-        private string myAdUnitId = WADUNITID;
 
         public BookPage()
         {
@@ -41,17 +43,30 @@ namespace MyShelf.Pages
             if (DeviceInformation.Instance.IsPhone)
             {
                 Windows.Phone.UI.Input.HardwareButtons.BackPressed += HardwareButtons_BackPressed;
-
-                myAppId = MAPPLICATIONID;
-                myAdUnitId = MADUNITID;
             }
 
+            SetUpAds();
+        }
+
+        private void SetUpAds()
+        {
             // Initialize the AdControl.
             adControl = new AdControl();
-            adControl.ApplicationId = myAppId;
-            adControl.AdUnitId = myAdUnitId;
-            adControl.Width = AD_WIDTH;
-            adControl.Height = AD_HEIGHT;
+
+            if (DeviceInformation.Instance.IsPhone)
+            {
+                adControl.ApplicationId = MAPPLICATIONID;
+                adControl.AdUnitId = MADUNITID;
+                adControl.Width = AD_WIDTH_MOBILE;
+                adControl.Height = AD_HEIGHT_MOBILE;
+            }
+            else
+            {
+                adControl.ApplicationId = WAPPLICATIONID;
+                adControl.AdUnitId = WADUNITID;
+                adControl.Width = AD_WIDTH;
+                adControl.Height = AD_HEIGHT;
+            }
             adControl.IsAutoRefreshEnabled = true;
 
             adGrid.Children.Add(adControl);
